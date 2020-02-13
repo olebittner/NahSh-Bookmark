e8d38c6757c6285ee7199415c03ab41b15b373a5
Ole Bittner <dev@oleb.it>
2020-02-13 19:32:38 +0100

    implement title

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,41 @@ const hafas = require('nahsh-hafas');
 const dateFormat = require('dateformat');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/stations', function(req, res, next) {
-  hafas.locations(req.query.s, {addresses: false, poi: false, linesOfStops: true, language: 'de'})
-      .then(function (locations) {
-        //res.send(locations);
-        res.render('stations', {stations: locations});
-      });
+router.get('/stations', function (req, res, next) {
+    hafas.locations(req.query.s, {addresses: false, poi: false, linesOfStops: true, language: 'de'})
+        .then(function (locations) {
+            //res.send(locations);
+            res.render('stations', {stations: locations});
+        });
 });
 
-router.get('/departures', function(req, res, next) {
-  var time = new Date();
-  if (req.query.time) {
-    const h = parseInt(req.query.time.substr(0, 2));
-    const min = parseInt(req.query.time.substr(2, 2));
-    if (!isNaN(h) && !isNaN(min))
-      time.setHours(h, min, 0);
-  }
-  var dur = parseInt(req.query.d);
-  if (isNaN(dur))
-    dur = 30;
-  hafas.departures(req.query.id, {when: time, duration: dur, language: 'de'})
-      .then(function (departures) {
-        //res.send(departures);
-        res.render('departures', {departures: departures, dateFormat: dateFormat});
-      });
+router.get('/departures', function (req, res, next) {
+    let time = new Date();
+    if (req.query.time) {
+        const h = parseInt(req.query.time.substr(0, 2));
+        const min = parseInt(req.query.time.substr(2, 2));
+        if (!isNaN(h) && !isNaN(min))
+            time.setHours(h, min, 0);
+    }
+    let dur = parseInt(req.query.d);
+    if (isNaN(dur))
+        dur = 30;
+    hafas.departures(req.query.id, {when: time, duration: dur, language: 'de'})
+        .then(function (departures) {
+            let title = "Departures";
+            if (departures.length > 0)
+                title = departures[0].station.name || title;
+            res.render('departures',
+                {
+                    title: title,
+                    departures: departures,
+                    dateFormat: dateFormat
+                });
+        });
 });
 
 module.exports = router;
